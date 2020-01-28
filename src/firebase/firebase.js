2dd68f130ac7697c9b515f6c66cdbbd4fadff012
Siliang Chang <siliang.chang@gmail.com>
2020-01-27 23:57:53 -0700

    setup env

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,13 +1,13 @@
 import * as firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDkMGB_8PxoBmUOVSAKAK2gFs16opCA0Uw",
-  authDomain: "budget-tracker-c53c3.firebaseapp.com",
-  databaseURL: "https://budget-tracker-c53c3.firebaseio.com",
-  projectId: "budget-tracker-c53c3",
-  storageBucket: "budget-tracker-c53c3.appspot.com",
-  messagingSenderId: "340588271212",
-  appId: "1:340588271212:web:ba4814d9f43913046faa49"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID
 };
 
 // Initialize Firebase
